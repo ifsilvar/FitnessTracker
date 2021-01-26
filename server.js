2fcc94c21b0184039c01f774d7ed567e02f4794d
Ivan Flores <ifsilvar4@gmail.com>
2021-01-26 00:02:56 -0800

    fixed db connection

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise", {
-    useNewUrlParser: true, useUnifiedTopology: true 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 app.listen(PORT, () => {
