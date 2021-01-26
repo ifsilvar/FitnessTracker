5a2753fe316f73f36fa981c7b29ff02b18bbe4bd
Ivan Flores <ifsilvar4@gmail.com>
2021-01-25 16:57:03 -0800

    make it better

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,35 @@
 const router = require("express").Router();
-const Transaction = require("../models/workout.js");
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+      .then(workout => {
+        res.json(workout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.post("/api/workout/bulk", ({ body }, res) => {
+    Workout.insertMany(body)
+      .then(workout => {
+        res.json(workout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.get("/api/workout", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(workout => {
+        res.json(workout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  module.exports = router;
